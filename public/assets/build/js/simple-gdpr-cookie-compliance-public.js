5d5c92199ad1f4085e8a1f913fea686c6c25c146
Anuj <stoneera111@gmail.com>
2019-11-24 17:04:44 +0545

    Update simple-gdpr-cookie-compliance-public.js

--- public/assets/build/js/simple-gdpr-cookie-compliance-public.js
+++ public/assets/build/js/simple-gdpr-cookie-compliance-public.js
@@ -1,1 +1,17 @@
-!function(){"use strict";jQuery(document).ready(function(){})}();
+(function($) {
+
+    'use strict';
+
+    // Document ready function
+
+    $(document).ready(function() {
+
+        $('.close-sgcc').on('click', function() {
+
+            $('.sgcc-main-wrapper').hide();
+
+        });
+
+    });
+
+})(jQuery);
