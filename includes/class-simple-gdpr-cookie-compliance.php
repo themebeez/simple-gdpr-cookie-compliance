c77e23090099f1f8529bb7242bb41f24318de106
Anuj Subedi <struggleage@gmail.com>
2025-01-30 12:41:58 +0545

    :package: Cookie name.
    - :package: Cookie name.

--- includes/class-simple-gdpr-cookie-compliance.php
+++ includes/class-simple-gdpr-cookie-compliance.php
@@ -217,7 +217,7 @@ class Simple_GDPR_Cookie_Compliance {
 
 		$plugin_public = new Simple_GDPR_Cookie_Compliance_Public( $this->get_plugin_name(), $this->get_version() );
 
-		if ( ! isset( $_COOKIE['s_gdpr_c_c_cookie'] ) ) {
+		if ( ! isset( $_COOKIE['sgcc-cookie-notice'] ) ) {
 			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 			$this->loader->add_action( 'wp_footer', $plugin_public, 'display_notice' );
